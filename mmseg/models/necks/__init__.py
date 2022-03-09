711b3d3a683be592689a9b1d6a4b6d82e9a67245
AnonymousECCV <101050701+AnonymousECCV@users.noreply.github.com>
2022-03-09 16:47:53 +0800

    Update __init__.py

--- mmseg/models/necks/__init__.py
+++ mmseg/models/necks/__init__.py
@@ -4,6 +4,6 @@ from .ic_neck import ICNeck
 from .jpu import JPU
 from .mla_neck import MLANeck
 from .multilevel_neck import MultiLevelNeck
-from .ullmc_ff import Cascade_FFM
+from .ullmc_ff import Cascade_TRP
 
-__all__ = ['FPN', 'MultiLevelNeck', 'MLANeck', 'ICNeck', 'JPU', 'Cascade_FFM']
+__all__ = ['FPN', 'MultiLevelNeck', 'MLANeck', 'ICNeck', 'JPU', 'Cascade_TRP']
