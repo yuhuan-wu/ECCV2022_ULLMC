bfdbdf6502b5d5875eb063e7db6d043046ec3559
AnonymousECCV <101050701+AnonymousECCV@users.noreply.github.com>
2022-03-09 16:24:06 +0800

    Create ullmc_sd.py

---
+++ mmseg/models/decode_heads/ullmc_sd.py
@@ -0,0 +1,25 @@
+# Segmentation Decoder
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from mmcv.cnn import ConvModule
+import math
+import cv2
+import numpy as np
+import os
+# from mmseg.ops import resize
+from mmseg.models.builder import HEADS
+from mmcv.cnn import normal_init
+from mmcv.runner import auto_fp16, force_fp32
+
+from mmseg.core import build_pixel_sampler
+from mmseg.ops import resize
+from mmseg.models.builder import build_loss
+from mmseg.models.losses import accuracy
+
+from abc import ABCMeta, abstractmethod
+
+from mmseg.models.utils.layers import SelfIIWA, MS_CrossIIWA
+import torch.utils.checkpoint as checkpoint
+from einops import rearrange
